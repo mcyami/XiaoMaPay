dde31553709809c4f9fd198795a592173ab97758
mcyami <mcyami@163.com>
2024-12-20 16:08:12 +0800

    update: 查看通道手续费分账比例

--- app/admin/model/FeeRuleModel.php
+++ app/admin/model/FeeRuleModel.php
@@ -45,4 +45,67 @@ class FeeRuleModel extends BaseModel {
         return true;
     }
 
+    /**
+     * 获取通道手续费分账比例
+     * @param int $channelId
+     * @return array
+     */
+    public static function getRulesByChannelId(int $channelId): array {
+        // 获取该通道所有的分账规则
+        $rules = self::where('channel_id', $channelId)->get();
+        // 通过merchant_id分组
+        $rules = $rules->groupBy('merchant_id')->toArray();
+        $channelRules = [];
+        // 平台默认手续费收款商户ID
+        $platformMerchantId = C('PLATFORM_FEE_MERCHANT');
+
+        /* 通过规则生成分账比例 */
+        // 1、生成通道通用分账比例数组
+        $cent = 100;
+        $common_rule = []; // 通用规则
+        $total = 0; // 规则中设置的比例总和
+        if (isset($rules[0]) && !empty($rules[0])) {
+            foreach ($rules[0] as $r) {
+                $total = bcadd($total, $r['rate'], 2);
+                $common_rule[$r['receive_merchant_id']] = $r['rate'];
+            }
+            // 不足100%的部分，平台收取
+            if ($total < $cent) {
+                $common_rule[$platformMerchantId] = bcsub($cent, $total, 2);
+            }
+        } else {
+            // 通道没有设置分账规则，平台收取全部
+            $common_rule[$platformMerchantId] = $cent;
+        }
+        $channelRules["*"] = $common_rule;
+
+        // 2、生成指定商户分账比例，基于通用分账比例计算，中间人分账从指定平台商户ID中扣去
+        if (count($rules) > 1) {
+            foreach ($rules as $merchantId => $rule) {
+                if ($merchantId == 0) {
+                    continue;
+                }
+                $merchant_rule = $common_rule;
+                foreach ($rule as $r) {
+                    $nowPlatformId = $r['platform_id'];
+                    $nowPlatformRate = $merchant_rule[$nowPlatformId] ?? 0;
+                    // 指定平台商户ID在通用规则中的分成 > 当前中间人分成，则平台商户ID分成减去中间人分成，减去部分给中间人
+                    if ($nowPlatformRate) {
+                        if ($nowPlatformRate > $r['rate']) {
+                            // 平台分成 > 中间人分成，平台分成中减掉中间人分成，加入中间人分成
+                            $merchant_rule[$nowPlatformId] = bcsub($nowPlatformRate, $r['rate'], 2);
+                            $merchant_rule[$r['receive_merchant_id']] = $r['rate'];
+                        } else {
+                            // 平台分成 < 中间人分成，平台分成全部给中间人，去掉平台分成
+                            $merchant_rule[$r['receive_merchant_id']] = $nowPlatformRate;
+                            unset($merchant_rule[$nowPlatformId]);
+                        }
+                    }
+                }
+                $channelRules[$merchantId] = $merchant_rule;
+            }
+        }
+        return $channelRules;
+    }
+
 }
